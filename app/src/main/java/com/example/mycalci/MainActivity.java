6d1b21d3d316a787dfe25c77ea804ebdff3bb9ad
Riddhii-21 <128205483+Riddhii-21@users.noreply.github.com>
2023-05-18 21:46:05 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/mycalci/MainActivity.java
+++ app/src/main/java/com/example/mycalci/MainActivity.java
@@ -5,10 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    TextView workingsTV;
+    TextView resultsTV;
+    
+    String workings = "";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTextViews();
+    }
+    private void initTextViews()
+    {
+        workingsTV = (TextView)findViewById(R.id.workingTextView);
+        resultsTV = (TextView)findViewById(r.id.resultTextView);
     }
 }
